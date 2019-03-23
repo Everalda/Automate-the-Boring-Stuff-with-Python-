8e42710579c1509833b929eaad0245084aeea0d0
everalda <esms@ecomp.poli.br>
2019-03-23 13:00:34 -0300

    data structures- list of dictionaries

---
+++ dataStructure.py
@@ -0,0 +1,12 @@
+#dictionary
+cat = { 'name':'zophie','age':9,'color':'gray'}
+
+#list of dictionaries
+
+allCats = []
+allCats.append({'name':'zophie','age':9,'color':'gray'})
+allCats.append({'name':'pooka','age':7,'color':'black'})
+allCats.append({'name':'fat-tail','age':5,'color':'gray'})
+allCats.append({'name': '???','age': -1, 'color': 'orange'})
+
+print(allCats)
