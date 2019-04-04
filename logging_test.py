b1f2ef98ddc4e93adc3284bc1caf7a840ffadbbb
everalda <esms@ecomp.poli.br>
2019-04-03 21:26:45 -0300

    this is just a test
    body of commit

--- logging_test.py
+++ logging_test.py
@@ -1,16 +1,17 @@
 import logging
-logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(levelname)s - %(message)s')
-#logging.debug('Start of Program')
+logging.basicConfig(filename='myprogramLog.txt',level = logging.DEBUG, format = '%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of Program')
 logging.disable(logging.CRITICAL)
+
 def factorial(n):
     total = 1
     logging.debug('Start of factorial(%s)' % (n))
+   
     for i in range(1,n+1):
         total*=i
         logging.debug('i is %s,total is %s' %(i,total))
     return total
-logging.debug('End of Factorial')
 
+logging.debug('End of Factorial')
 print(factorial(5))
 
- 
